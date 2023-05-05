7135ae8987862c99011bc1acfadf3882b89c403a
Porpi01 <126757988+Porpi01@users.noreply.github.com>
2023-05-05 18:38:32 +0200

    Add files via upload

---
+++ almacen.sql
@@ -0,0 +1,63 @@
+-- -- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema almacen
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema almacen
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `almacen` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
+USE `almacen` ;
+
+-- -----------------------------------------------------
+-- Table `almacen`.`categoria`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `almacen`.`categoria` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `almacen`.`producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `almacen`.`producto` (
+  `idproducto` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `precio` DECIMAL NOT NULL,
+  `existencias` INT NOT NULL,
+  `categoria_id` INT NOT NULL,
+  PRIMARY KEY (`idproducto`),
+  INDEX `fk_producto_categoria_idx` (`categoria_id` ASC) VISIBLE,
+  CONSTRAINT `fk_producto_categoria`
+    FOREIGN KEY (`categoria_id`)
+    REFERENCES `almacen`.`categoria` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+INSERT INTO `categoria` (`id`, `nombre`) VALUES
+(1, 'Bebidas'),
+(2, 'Condimentos'),
+(3, 'Repostería'),
+(4, 'Huevos y Lácteos'),
+(5, 'Granos y Cereales'),
+(6, 'Carnes'),
+(7, 'Frutas y Verduras'),
+(8, 'Pescado y Marisco'),
+(9, 'Congelados'),
+(10, 'Limpieza');
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
